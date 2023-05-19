5d64384e7bec68a3763ac73284da9466696d8e69
crenaz <crenaz@gmail.com>
2023-05-19 16:11:15 -0400

    first commit

---
+++ src/config.mjs
@@ -0,0 +1,38 @@
+export const SITE = {
+	name: 'AgustinCrespo',
+
+	origin: 'https://agustincrespo.com',
+	basePathname: '/',
+	trailingSlash: false,
+
+	title: 'AgustinCrespo.com',
+	description: 'Portfolio website for Agustin Crespo built with Astro 🚀 and TailWind.',
+
+	googleAnalyticsId: false, // or "G-XXXXXXXXXX",
+	//googleSiteVerificationId: 'orcPxI47GSa-cRvY11tUe6iGg2IO_RPvnA1q95iEM3M',
+};
+
+export const BLOG = {
+	disabled: false,
+	postsPerPage: 4,
+
+	blog: {
+		disabled: false,
+		pathname: 'blog', // blog main path, you can change this to "articles" (/articles)
+	},
+
+	post: {
+		disabled: false,
+		pathname: '', // empty for /some-post, value for /pathname/some-post
+	},
+
+	category: {
+		disabled: false,
+		pathname: 'category', // set empty to change from /category/some-category to /some-category
+	},
+
+	tag: {
+		disabled: false,
+		pathname: 'tag', // set empty to change from /tag/some-tag to /some-tag
+	},
+};
