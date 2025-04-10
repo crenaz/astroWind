132c329fecefda11eb604389a81ce129d27d1f09
crenaz <crenaz@gmail.com>
2025-04-10 14:12:08 -0400

    astro v2 to v3

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,36 +8,46 @@ import mdx from '@astrojs/mdx';
 import partytown from '@astrojs/partytown';
 import { remarkReadingTime } from './src/utils/frontmatter.mjs';
 import { SITE } from './src/config.mjs';
-import react from "@astrojs/react";
+import react from '@astrojs/react';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
-const whenExternalScripts = (items = []) => SITE.googleAnalyticsId ? Array.isArray(items) ? items.map(item => item()) : [items()] : [];
+const whenExternalScripts = (items = []) =>
+	SITE.googleAnalyticsId ? (Array.isArray(items) ? items.map((item) => item()) : [items()]) : [];
 
 // https://astro.build/config
 export default defineConfig({
-  site: SITE.origin,
-  base: SITE.basePathname,
-  trailingSlash: SITE.trailingSlash ? 'always' : 'never',
-  output: 'static',
-  integrations: [tailwind({
-    config: {
-      applyBaseStyles: false
-    }
-  }), sitemap(), image({
-    serviceEntryPoint: '@astrojs/image/sharp'
-  }), mdx(), ...whenExternalScripts(() => partytown({
-    config: {
-      forward: ['dataLayer.push']
-    }
-  })), react()],
-  markdown: {
-    remarkPlugins: [remarkReadingTime],
-    extendDefaultPlugins: true
-  },
-  vite: {
-    resolve: {
-      alias: {
-        '~': path.resolve(__dirname, './src')
-      }
-    }
-  }
+	site: SITE.origin,
+	base: SITE.basePathname,
+	trailingSlash: SITE.trailingSlash ? 'always' : 'never',
+	output: 'static',
+	integrations: [
+		tailwind({
+			config: {
+				applyBaseStyles: false,
+			},
+		}),
+		sitemap(),
+		image({
+			entrypoint: '@astrojs/image/sharp', // Updated from serviceEntryPoint
+		}),
+		mdx(),
+		...whenExternalScripts(() =>
+			partytown({
+				config: {
+					forward: ['dataLayer.push'],
+				},
+			})
+		),
+		react(),
+	],
+	markdown: {
+		remarkPlugins: [remarkReadingTime],
+		extendDefaultPlugins: true,
+	},
+	vite: {
+		resolve: {
+			alias: {
+				'~': path.resolve(__dirname, './src'),
+			},
+		},
+	},
 });
